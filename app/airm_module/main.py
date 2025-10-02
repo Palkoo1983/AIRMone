66c2e28881f865f3103801e496a669779a66cafa
Palkoo1983 <paulsdiamond@gmail.com>
2025-10-02 17:20:29 +0200

    Update main.py

--- app/airm_module/main.py
+++ app/airm_module/main.py
@@ -12,6 +12,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+import re, json, math
+from typing import Any, Dict, List, Optional, Tuple, Set, Iterable, Union, Callable
 
 app = FastAPI(
     title="AIRM backend",
